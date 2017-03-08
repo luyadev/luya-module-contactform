409a2d4ee1acace95b87ea44db0e53cf4a1c72df
Basil <git@nadar.io>
2017-03-08 10:06:31 +0100

    Update DefaultController.php

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -51,7 +51,7 @@ class DefaultController extends \luya\web\Controller
                 
                 return $this->refresh();
             } else {
-            	throw new InvalidConfigException('Unable to send email, maybe the error component was not configure propertyl.');
+            	throw new InvalidConfigException('Unable to send email, maybe the mail component is not configure properly.');
             }
         } else {
             // as the toolbar maybe try's to re render this part of the controller.
